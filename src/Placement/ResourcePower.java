b95e1b6a3c3f91e5d2be3f0e31dd2a642a74da08
Imthiyaz Husain <imthiyazhusain02.09.2005@gmail.com>
2026-02-23 15:52:07 +0530

    placement

--- src/Placement/ResourcePower.java
+++ src/Placement/ResourcePower.java
@@ -3,13 +3,12 @@ package Placement;
 import java.util.Scanner;
 
 public class ResourcePower {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        int count = 0;
-        count += Math.min(a, Math.min(b, c));
+        int count = Math.min(a, Math.min(b, c));
         a-=count;
         b-=count;
         c-=count;
@@ -17,13 +16,6 @@ public class ResourcePower {
         count += a/3;
         count += b/3;
         count += c/3;
-
-//        while(a!=0 && b!=0 && c!=0){
-//            a--;
-//            b--;
-//            c--;
-//            count++;
-//        }
 
         System.out.println(count);
     }
