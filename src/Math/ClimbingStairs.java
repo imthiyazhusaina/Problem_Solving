909bca1568681c1c8cb7b753b812668f23299951
YourName  <imthiyazhusaina@gmail.com>
2026-07-04 21:13:10 +0530

    Climbing Satirs

--- src/Math/ClimbingStairs.java
+++ src/Math/ClimbingStairs.java
@@ -1,4 +1,25 @@
 package Math;
 
+import java.util.Scanner;
+
 public class ClimbingStairs {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n==0){
+            System.out.println(0);
+        }
+        if(n==1){
+            System.out.println(1);
+        }
+        int a = 0;
+        int b = 1;
+        int c = a+b;
+        for(int i=1;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        System.out.println(c);
+    }
 }
