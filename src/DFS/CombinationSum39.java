8e9503b50515f088be6cab14407d640e9684b3e6
YourName  <imthiyazhusaina@gmail.com>
2026-07-08 22:48:19 +0530

    DFS Combination

--- src/DFS/CombinationSum39.java
+++ src/DFS/CombinationSum39.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class CombinationSum39 {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] candidates = {2,2,5,7};
+        int[] candidates = {1,3,6,7};
         int target = 7;
         Solution39 c = new Solution39();
         System.out.println(c.combinationSum(candidates,target));
@@ -13,15 +13,13 @@ class Solution39 {
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-
         backtrack(candidates, target, 0, new ArrayList<>(), ans);
-
         return ans;
     }
 
     private void backtrack(int[] candidates, int target, int index,
                            List<Integer> current, List<List<Integer>> ans) {
-
+        System.out.println(current);
         // Found a valid combination
         if (target == 0) {
             ans.add(new ArrayList<>(current));
@@ -41,7 +39,7 @@ class Solution39 {
             backtrack(candidates, target - candidates[i], i, current, ans);
 
             // Backtrack
-            current.remove(current.size() - 1);
+            current.removeLast();
         }
     }
 }
