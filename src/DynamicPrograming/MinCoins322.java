80aed85904a385b3d1d56445f3172a8fa4869b6b
YourName  <imthiyazhusaina@gmail.com>
2026-07-09 23:09:18 +0530

    DP Coins optimal

--- src/DynamicPrograming/MinCoins322.java
+++ src/DynamicPrograming/MinCoins322.java
@@ -11,26 +11,52 @@ public class MinCoins322 {
 class Solution322 {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n+1][amount+1];
-        Arrays.fill(dp[0],-1);
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<amount+1;j++){
-                int d = j-coins[i-1];
-                if(j>=coins[i-1]){
-                    if(dp[i-1][j]==-1 && dp[i][d]==-1){
-                        dp[i][j] = dp[i-1][j];
-                    }else if(dp[i-1][j] == -1 && dp[i][d]!=-1){
-                        dp[i][j] = dp[i][d]+1;
-                    }else if(dp[i-1][j] != -1 && dp[i][d]==-1){
-                        dp[i][j] = dp[i-1][j];
-                    }else{
-                        dp[i][j] = Math.min(dp[i-1][j],dp[i][d]+1);
-                    }
-                }else{
-                    dp[i][j] = dp[i-1][j];
+        int INF = amount + 1;
+
+        int[][] dp = new int[n + 1][amount + 1];
+
+        // Base initialization
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], INF);
+            dp[i][0] = 0;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+
+                // Don't take current coin
+                dp[i][j] = dp[i - 1][j];
+
+                // Take current coin (unlimited times)
+                if (j >= coins[i - 1]) {
+                    dp[i][j] = Math.min(dp[i][j],
+                            dp[i][j - coins[i - 1]] + 1);
                 }
             }
         }
-        return dp[n][amount];
+
+        return dp[n][amount] == INF ? -1 : dp[n][amount];
+//        int n = coins.length;
+//        int[][] dp = new int[n+1][amount+1];
+//        Arrays.fill(dp[0],-1);
+//        for(int i=1;i<n+1;i++){
+//            for(int j=1;j<amount+1;j++){
+//                int d = j-coins[i-1];
+//                if(j>=coins[i-1]){
+//                    if(dp[i-1][j]==-1 && dp[i][d]==-1){
+//                        dp[i][j] = dp[i-1][j];
+//                    }else if(dp[i-1][j] == -1 && dp[i][d]!=-1){
+//                        dp[i][j] = dp[i][d]+1;
+//                    }else if(dp[i-1][j] != -1 && dp[i][d]==-1){
+//                        dp[i][j] = dp[i-1][j];
+//                    }else{
+//                        dp[i][j] = Math.min(dp[i-1][j],dp[i][d]+1);
+//                    }
+//                }else{
+//                    dp[i][j] = dp[i-1][j];
+//                }
+//            }
+//        }
+//        return dp[n][amount];
     }
 }
