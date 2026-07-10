15e8dbd5c5f35ecd145113038a3f54a517a08bbe
YourName  <imthiyazhusaina@gmail.com>
2026-07-10 21:28:00 +0530

    416 partition sum with Dp

--- src/DynamicPrograming/PartitionEqualSubsetSum416.java
+++ src/DynamicPrograming/PartitionEqualSubsetSum416.java
@@ -15,19 +15,28 @@ class Solution416 {
         }
         if(sum%2==1) return false;
         sum=sum/2;
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
 
-         boolean[][] dp = new boolean[n+1][sum+1];
-         for(int i=0;i<n+1;i++){
-             dp[i][0] = true;
-         }
-         for(int i=1;i<n+1;i++){
-             for(int j=1;j<sum+1;j++){
-                 dp[i][j] = dp[i-1][j];
-                 if(j>=nums[i-1]){
-                     dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
-                 }
-             }
-         }
-         return dp[n][sum];
+        for (int num : nums) {
+            for (int j = sum; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+        return dp[sum];
+
+//         boolean[][] dp = new boolean[n+1][sum+1];
+//         for(int i=0;i<n+1;i++){
+//             dp[i][0] = true;
+//         }
+//         for(int i=1;i<n+1;i++){
+//             for(int j=1;j<sum+1;j++){
+//                 dp[i][j] = dp[i-1][j];
+//                 if(j>=nums[i-1]){
+//                     dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+//                 }
+//             }
+//         }
+//         return dp[n][sum];
     }
 }
