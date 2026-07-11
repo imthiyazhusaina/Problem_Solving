f7146cf2ab1ce8613f9845b99a439a3537ef7b69
YourName  <imthiyazhusaina@gmail.com>
2026-07-11 20:36:04 +0530

    List DP

---
+++ src/DynamicPrograming/Combination39.java
@@ -0,0 +1,14 @@
+package DynamicPrograming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combination39 {
+    static void main(String[] args) {
+        List<List<Integer>> list = new ArrayList<>();
+        int[] arr = {2,3,6,7};
+        int target = 7;
+        List<Integer>[] dp = new ArrayList[target+1];
+
+    }
+}
