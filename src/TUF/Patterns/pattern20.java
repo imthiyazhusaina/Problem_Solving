37c43f3a5ae4148b381fe820519693f0eda9d566
imthiyazhusaina <imthiyazhusaina@gmail.com>
2026-07-26 21:13:02 +0530

    TUF pattern

---
+++ src/TUF/Patterns/pattern20.java
@@ -0,0 +1,44 @@
+package TUF.Patterns;
+
+// Given an integer n. You need to recreate the pattern
+// given below for any value of N. Let's say for N = 5,
+// the pattern should look like as below:
+//        *        *
+//        **      **
+//        ***    ***
+//        ****  ****
+//        **********
+//        ****  ****
+//        ***    ***
+//        **      **
+//        *        *
+
+public class pattern20 {
+    static void main() {
+        int n = 5;
+        for(int i = 0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<n*2-(i*2)-2;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = 0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<i*2+2;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<n-i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
