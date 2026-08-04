1afd6fec9c12d82387be0aa34763d374a630a74c
imthiyazhusaina <imthiyazhusaina@gmail.com>
2026-08-04 16:24:06 +0530

    GCD

--- src/TUF/BasicMath/GCD.java
+++ src/TUF/BasicMath/GCD.java
@@ -1,4 +1,5 @@
 package TUF.BasicMath;
+import java.util.*;
 
 //    Example 1:
 //    Input: N1 = 9, N2 = 12
@@ -21,4 +22,27 @@ package TUF.BasicMath;
 //    Greatest common factor: 5 (GCD)
 
 public class GCD {
+    public static int findGcd(int a, int b) {
+        while(a > 0 && b > 0) {
+            if(a > b) {
+                a = a % b;
+            }
+            else {
+                b = b % a;
+            }
+        }
+        if(a == 0) {
+            return b;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int n1 = 20, n2 = 15;
+
+        // Find the GCD of n1 and n2
+        int gcd = findGcd(n1, n2);
+
+        System.out.println("GCD of " + n1 + " and " + n2 + " is: " + gcd);
+    }
 }
